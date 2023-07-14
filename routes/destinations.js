dcca8ba027d728836cd009a621bdf13dae8ab77c
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-07-14 07:32:49 -0700

    new.ejs have questions and also idk if i created my async function correctly. can we go over

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-
 // You Do - Require the yet to be created reviews controller 
+const destinationsCtrl = require('../controllers/destinations');
+
+
+// POST /movies/:id/reviews (create review for a movie)
+router.post('/flights/:id/destinations', destinationsCtrl.create);
 
 
 // You Do - Define the Route below
