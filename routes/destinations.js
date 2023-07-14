fe6195c4210e7c38b74277f6f20b18c60b61fcc5
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-07-14 07:04:06 -0700

    created destination controller, but show vie is is not displaying

---
+++ routes/destinations.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+// You Do - Require the yet to be created reviews controller 
+
+
+// You Do - Define the Route below
+
+
+
+
+
+module.exports = router;
